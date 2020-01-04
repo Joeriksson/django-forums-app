1179bd4bb75066c10ab915d701919ded70d8c562
J-O Eriksson <joeriksson@hemdatorn.se>
2020-01-04 15:41:48 +0100

    Added permissions check to UserProfiles

--- project/views.py
+++ project/views.py
@@ -1,11 +1,16 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from forums.forms import UserProfileForm
 from forums.models import UserProfile
 
 
-class UserProfileUpdate(UpdateView):
+class UserProfileUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = UserProfile
     success_url = reverse_lazy('home')
     form_class = UserProfileForm
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
