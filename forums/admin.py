c6b653ca8f040f114bf51cc923e3a6b7ab49522a
Jan-Olov Eriksson <janeri@C02YT1VSLVCH.local>
2020-03-23 15:09:51 +0100

    Enhancements to the admin interfaces

--- forums/admin.py
+++ forums/admin.py
@@ -2,9 +2,36 @@ from django.contrib import admin
 
 from .models import Forum, Thread, Post, UserProfile, UpVote, Notification
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('thread', 'added', 'edited', 'user', 'upvotes')
+    list_filter = (
+        ('added', admin.DateFieldListFilter),
+        ('edited', admin.DateFieldListFilter),
+    )
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('thread', 'user')
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'forum', 'added', 'edited', 'user')
+    list_filter = (
+        ('forum', admin.RelatedFieldListFilter),
+        ('added', admin.DateFieldListFilter),
+        ('edited', admin.DateFieldListFilter),
+    )
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','first_name', 'last_name', 'gender', 'location')
+    list_filter = (
+        ('location', )
+    )
+
 admin.site.register(Forum)
-admin.site.register(Thread)
-admin.site.register(Post)
-admin.site.register(UserProfile)
+admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(UpVote)
-admin.site.register(Notification)
+admin.site.register(Notification, NotificationAdmin)
--- users/admin.py
+++ users/admin.py
@@ -3,15 +3,28 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from forums.models import UserProfile
 
 CustomUser = get_user_model()
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username']
+    inlines = [UserProfileInline]
+    list_display = ['email', 'username', 'is_staff', 'is_active','date_joined']
+    # list_filter = ('date_joined',)
+
+    list_filter = (
+        ('is_staff', admin.BooleanFieldListFilter),
+        ('is_superuser', admin.BooleanFieldListFilter),
+        ('is_active', admin.BooleanFieldListFilter),
+        ('date_joined', admin.DateFieldListFilter),
+    )
+
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
