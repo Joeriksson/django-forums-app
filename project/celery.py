56bb47ee2fd0d097c07476213cd507708666a50e
Jan-Olov Eriksson <janeri@C02YT1VSLVCH.local>
2020-10-15 17:06:45 +0200

    Change settings file used

--- project/celery.py
+++ project/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
 
 app = Celery('project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
