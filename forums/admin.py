2e34a8b251427b2c8fc77d08f0911d1e586da087
Jan-Olov Eriksson <janeri@C02YT1VSLVCH.local>
2020-10-16 09:06:40 +0200

    Enhance admin

--- forums/admin.py
+++ forums/admin.py
@@ -4,18 +4,25 @@ from .models import Forum, Thread, Post, UserProfile, UpVote, Notification
 
 
 class PostAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
     list_display = ('thread', 'added', 'edited', 'user', 'upvotes')
     list_filter = (
         ('added', admin.DateFieldListFilter),
         ('edited', admin.DateFieldListFilter),
+        ('upvotes', admin.BooleanFieldListFilter),
     )
 
 
 class NotificationAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
+    list_filter = (
+        ('added', admin.DateFieldListFilter),
+    )
     list_display = ('thread', 'user')
 
 
 class ThreadAdmin(admin.ModelAdmin):
+    search_fields = ('user__username',)
     list_display = ('title', 'forum', 'added', 'edited', 'user')
     list_filter = (
         ('forum', admin.RelatedFieldListFilter),
@@ -23,11 +30,15 @@ class ThreadAdmin(admin.ModelAdmin):
         ('edited', admin.DateFieldListFilter),
     )
 
+
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','first_name', 'last_name', 'gender', 'location')
+    search_fields = ('first_name', 'last_name', 'user__username')
+    list_display = ('user', 'first_name', 'last_name', 'gender', 'location')
     list_filter = (
-        ('location', )
+        'location',
+        'gender',
     )
+
 
 admin.site.register(Forum)
 admin.site.register(Thread, ThreadAdmin)
